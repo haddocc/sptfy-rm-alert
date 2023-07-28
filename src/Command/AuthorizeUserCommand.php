b9a1ff81602c665fbb23b9d9dc1ab4664023e54d
Ogier Schelvis <ogierschelvis@gmail.com>
2023-07-28 23:17:30 +0200

    spent 1h: - made OAuthClient stateless, because we're using it in the command - fix localhost redirect_uri generation - changed email to identifier to better anonymize the cache key, since I'm planning to provide it in the query string of the redirect uri, so I can store the access_token to the right cache key.

--- src/Command/AuthorizeUserCommand.php
+++ src/Command/AuthorizeUserCommand.php
@@ -8,12 +8,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Routing\RouterInterface;
 
 // the name of the command is what users type after "php bin/console"
 #[AsCommand(name: 'app:authorize-user', description: 'Authorizes a new user.', aliases: ['app:auth'])]
 class AuthorizeUserCommand extends Command
 {
-    public function __construct(private readonly ClientRegistry $clientRegistry, string $name = null)
+    public function __construct(private readonly ClientRegistry $clientRegistry, private readonly RouterInterface $router, string $name = null)
     {
         parent::__construct($name);
     }
@@ -26,24 +27,27 @@ class AuthorizeUserCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        // set router context to get proper redirect uri base url for a local env
+        $context = $this->router->getContext();
+        $context->setHttpsPort(8000);
+
         $helper = $this->getHelper('question');
 
-        $emailQuestion = new Question('Please fill in email address of your Spotify account: ', false);
+        $emailQuestion = new Question('Please provide a unique identifier to store the access token to your Spotify account: ', false);
 
-        //TODO: email validation
         $email = $helper->ask($input, $output, $emailQuestion);
 
         if($email) {
-            $output->writeln('You entered: ' . $email);
-            $this->initOauth2();
+            $this->initOAuth2($output);
         } else {
-            $output->writeln('No email given. exiting...');
+            $output->writeln('No identifier given. exiting...');
         }
         return Command::SUCCESS;
     }
 
-    protected function initOauth2(): void
+    protected function initOAuth2(OutputInterface $output): void
     {
-        dump($this->clientRegistry->getClient('spotify')->redirect(['user-library-read']));
+        $redirectResponse = $this->clientRegistry->getClient('spotify')->redirect(['scope' => 'user-library-read']);
+        $output->writeln('Please visit ' . $redirectResponse->getTargetUrl() . ' to grant permissions to read your library of liked tracks.');
     }
 }
--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use Kerox\OAuth2\Client\Provider\Exception\SpotifyIdentityProviderException;
 use Kerox\OAuth2\Client\Provider\SpotifyResourceOwner;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Client\Provider\SpotifyClient;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,22 +20,16 @@ class OAuthController extends AbstractController
     #[Route("/connect/spotify/check", name:"connect.spotify.check")]
     public function connectSpotifyCheckAction(Request $request, ClientRegistry $clientRegistry): void
     {
-        // ** if you want to *authenticate* the user, then
-        // leave this method blank and create a Guard authenticator
-        // (read below)
-
         /** @var SpotifyClient $client */
         $client = $clientRegistry->getClient('spotify');
-
         try {
             /** @var SpotifyResourceOwner $user */
-            $user = $client->fetchUser();
-
+            $accessToken = $client->getAccessToken();
             // do something with all this new power!
             // e.g. $name = $user->getFirstName();
             dd($user);
             // ...
-        } catch (SpotifyIdentityProviderException $e) {
+        } catch (IdentityProviderException $e) {
             // something went wrong!
             // probably you should return the reason to the user
             dd($e->getMessage());
